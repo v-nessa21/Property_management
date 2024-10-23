ed78ad0751a6ed45b1cec71ab83cfe0b1775b3b5
Hussein-irahari123 <130802574+Hussein-irahari123@users.noreply.github.com>
2024-10-23 15:37:07 +0200

    Update admin.py

--- property_management/Property_app/admin.py
+++ property_management/Property_app/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 
 
 from .models import Property,Tenant,Unit,Lease
-admin.site.register(Property)
-admin.site.register(Tenant)
-admin.site.register(Unit)
-admin.site.register(Lease)
+admin.site.register(Property)    # property
+admin.site.register(Tenant)      # tenant
+admin.site.register(Unit)        # unit
+admin.site.register(Lease)       # lease
+
 # Register your models here.
